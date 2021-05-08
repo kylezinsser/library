35721a479ec9190e687429eafd9e15b25ba74784
Kyle Zinsser <kylezinsser@gmail.com>
2021-05-08 16:56:51 -0600

    Added additional tables and fields for the db structure

--- app/models.py
+++ app/models.py
@@ -6,9 +6,9 @@ from app import db, login
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    username = db.Column(db.Text, index=True, unique=True)
+    email = db.Column(db.Text, index=True, unique=True)
+    password_hash = db.Column(db.Text)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)    
@@ -26,72 +26,128 @@ def load_user(id):
 
 class Universe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text)
 
 
 class Series(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    universe_id = db.Column(db.Integer, db.ForeignKey('universe.id'))
+    title = db.Column(db.Text)
 
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    universe_id = db.Column(db.Integer, db.ForeignKey('universe.id'))
+    series_id = db.Column(db.Integer, db.ForeignKey('series.id'))
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
+    coauthor_id = db.Column(db.Integer, db.ForeignKey('author.id'))
+    title = db.Column(db.Text)
+
+
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.Text)
+    last_name = db.Column(db.Text)
 
 
 class Character(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    universe_id = db.Column(db.Integer, db.ForeignKey('universe.id'))
+    series_id = db.Column(db.Integer, db.ForeignKey('series.id'))
+    first_name = db.Column(db.Text)
+    last_name = db.Column(db.Text)
+    description = db.Column(db.Text)
 
 
 class CharacterAlias(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
+    alias = db.Column(db.Text)
+
+
+class Appearance(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
+
+    # Is this relationship too obtuse? Could cover instances where a character that "belongs" to one 
+    # series appears in another under a different name e.g. Taelien/Keras, Hoid/Wit, etc
+    alias_id = db.Column(db.Integer, db.ForeignKey('alias.id'))
 
 
 class Actor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.Text)
+    last_name = db.Column(db.Text)
+
+
+class Art(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    artist = db.Column(db.Text)
+    title = db.Column(db.Text)
+    description = db.Column(db.Text)
+    source = db.Column(db.Text)
 
 
 class Tag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30))
+    name = db.Column(db.Text)
+    tag_type_id = db.Column(db.Integer, db.ForeignKey('tag_type.id'), default=0)
+
+
+class TagType(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
 
 
 class ActorTag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'))
     actor_id = db.Column(db.Integer, db.ForeignKey('actor.id'))
+    tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'))
 
 
 class ArtTag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'))
     art_id = db.Column(db.Integer, db.ForeignKey('art.id'))
+    tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'))
 
 
 class BookTag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'))
     book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
+    tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'))
+
+
+class SeriesTag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    series_id = db.Column(db.Integer, db.ForeignKey('series.id'))
+    tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'))
+
+
+class UniverseTag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    universe_id = db.Column(db.Integer, db.ForeignKey('universe.id'))
+    tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'))
 
 
 class CharacterTag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'))
     character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
+    tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'))
 
 
 class Reference(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    key = db.Column(db.String(1024))
+    key = db.Column(db.Text)
+    private = db.Column(db.Boolean, default=False)
 
 
 class ActorRef(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     actor_id = db.Column(db.Integer, db.ForeignKey('actor.id'))
     reference_id = db.Column(db.Integer, db.ForeignKey('reference.id'))
-    
 
-# actor refers to a specific person that can be distinct from the reference
-# e.g. young actor and older actor may have particular images that lend to different types of characters
-# aren't the art refs and the art itself indistinguishable though? AKA do we need both tables?
-# What does one have that another doesn't? Art table could have author, tags, ??
+
 class ArtRef(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     art_id = db.Column(db.Integer, db.ForeignKey('art.id'))
